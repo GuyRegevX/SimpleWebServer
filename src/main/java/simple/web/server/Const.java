e6d1accff7a11920fb8867118b3d7e098d9d995c
GuyRegevX <guy.regev@outlook.com>
2025-05-28 16:12:37 +0300

    Refactor WebServer with constants and enhanced thread pool management; add graceful shutdown support.

---
+++ src/main/java/simple/web/server/Const.java
@@ -0,0 +1,9 @@
+package simple.web.server;
+
+public class Const {
+
+    public static final String SERVER_NAME = "SimpleWebServer";
+    public static final String SERVER_VERSION = "1.0";
+    public static final int SERVER_PORT = 9090;
+    public static final int SERVER_MAX_THREADS = 10;
+}
